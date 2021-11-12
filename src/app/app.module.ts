cfd591d7da7dcd1d9811a29a0c89e3135202865c
Enrique <enrique.hernandez@campusfp.es>
2021-11-12 11:07:59 +0100

    Añadido el firebase auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,38 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import {
+  AngularFireAuth,
+  AngularFireAuthModule,
+} from '@angular/fire/compat/auth';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCS-G-BE-8wWb48nPunBpM-dv1JSJLLmxk',
+  authDomain: 'hito2desarrollointerfaces.firebaseapp.com',
+  projectId: 'hito2desarrollointerfaces',
+  storageBucket: 'hito2desarrollointerfaces.appspot.com',
+  messagingSenderId: '38146617491',
+  appId: '1:38146617491:web:40d239af9c628ff0f95d66',
+  measurementId: 'G-VXJ59W9KHL',
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+  ],
+  providers: [
+    AngularFireAuth,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
