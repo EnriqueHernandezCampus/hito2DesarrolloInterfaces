e317302673c5fc6f288b4a72ae9a76465bbd837e
Enrique <enrique.hernandez@campusfp.es>
2021-11-12 12:14:05 +0100

    configurado login y register

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  usuario = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(4)]],
+  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    public alertCtrl: AlertController,
+    private router: Router
+  ) {}
 
   ngOnInit() {
   }
-
+  signin() {
+    this.auth
+      .registerUser(this.usuario.value.email, this.usuario.value.password)
+      .then((result) => {
+        this.router.navigate(['/tabs']);
+      })
+      .catch((err) => {
+        this.alertCtrl
+          .create({
+            header: 'Error',
+            subHeader: err.message,
+            buttons: ['Aceptar'],
+          })
+          .then((alert) => {
+            alert.present();
+          });
+      });
+  }
 }
